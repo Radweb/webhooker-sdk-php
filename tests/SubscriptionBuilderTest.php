67f46a3613a01f22a2ad7971957d3d7c059b33fe
Robb Lewis <me@therobb.com>
2016-08-24 15:43:26 +0100

    Events only, no legacy

--- tests/SubscriptionBuilderTest.php
+++ tests/SubscriptionBuilderTest.php
@@ -132,7 +132,7 @@ class SubscriptionBuilderTest extends TestCase
     }
 
     /** @test */
-    public function it_can_be_configured_with_basic_auth_details_and_events()
+    public function it_can_be_configured_with_events()
     {
         $format = 'application/blah';
         $tenantKey = 'account-1';
@@ -147,52 +147,7 @@ class SubscriptionBuilderTest extends TestCase
                 'tenant' => $tenantKey,
                 'url' => $deliveryUrl,
                 'secret' => $secret,
-                'events' => $events,
-                'auth' => [
-                    'username' => 'bob',
-                    'password' => 'qwerty',
-                ],
-            ])
-            ->andReturn([
-                'id' => 'XD',
-                'subscriber_id' => 'wij',
-                'format' => $format,
-                'tenant' => $tenantKey,
-                'url' => $deliveryUrl,
-                'uses_basic_auth' => true,
-                'legacy' => [],
-            ])
-            ->once()
-            ->getMock();
-
-        $subscription = (new SubscriptionBuilder($api, 'wij', $format, $tenantKey, $deliveryUrl, $secret))->onlyEvents($events)->basicAuth('bob', 'qwerty')->save();
-
-        $expected = new Subscription('XD', 'wij', $tenantKey, $format, $deliveryUrl);
-        $expected->setUsesBasicAuth(true);
-
-        $this->assertEquals($expected, $subscription);
-    }
-
-    /** @test */
-    public function it_can_be_configured_with_legacy_mode_details_and_events()
-    {
-        $format = 'application/blah';
-        $tenantKey = 'account-1';
-        $deliveryUrl = 'https://ffo.com/x';
-        $secret = 'blahblah';
-        $events = ['inspection.completed'];
-
-        $api = m::mock(ApiClient::class)
-            ->shouldReceive('send')
-            ->with('POST', '/subscribers/wij/subscriptions', [
-                'format' => $format,
-                'tenant' => $tenantKey,
-                'url' => $deliveryUrl,
-                'secret' => $secret,
-                'events' => $events,
-                'legacy' => [
-                    'payload' => 'p_reply',
-                ],
+                'events' => $events
             ])
             ->andReturn([
                 'id' => 'XD',
@@ -201,17 +156,14 @@ class SubscriptionBuilderTest extends TestCase
                 'tenant' => $tenantKey,
                 'url' => $deliveryUrl,
                 'uses_basic_auth' => false,
-                'legacy' => [
-                    'payload' => 'p_reply',
-                ],
+	            'legacy' => []
             ])
             ->once()
             ->getMock();
 
-        $subscription = (new SubscriptionBuilder($api, 'wij', $format, $tenantKey, $deliveryUrl, $secret))->onlyEvents($events)->legacyPayload('p_reply')->save();
+        $subscription = (new SubscriptionBuilder($api, 'wij', $format, $tenantKey, $deliveryUrl, $secret))->onlyEvents($events)->save();
 
         $expected = new Subscription('XD', 'wij', $tenantKey, $format, $deliveryUrl);
-        $expected->setLegacyPayload('p_reply');
 
         $this->assertEquals($expected, $subscription);
     }
